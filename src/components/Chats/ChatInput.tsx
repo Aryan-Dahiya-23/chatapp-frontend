2d73c7f7d07e9390bd147fc5a79d11c04d94c132
unknown <aryan23dahiya@gmail.com>
2023-11-18 17:51:23 +0530

    changed width

--- src/components/Chats/ChatInput.tsx
+++ src/components/Chats/ChatInput.tsx
@@ -123,7 +123,7 @@ const ChatInput: React.FC<ChatInputProps> = ({ data, id }) => {
 
             <textarea
                 placeholder="Write a message"
-                className="textarea textarea-bordered text-base textarea-sm w-full resize-none leading-normal custom-scrollbar"
+                className="textarea textarea-bordered text-base textarea-sm w-11/12 resize-none leading-normal custom-scrollbar"
                 onChange={handleTextareaChange}
                 onKeyDown={handleKeyDown}
                 value={text}
