6c942bb6d15eb057ecc288894362bb8858ce4186
unknown <aryan23dahiya@gmail.com>
2023-11-24 20:48:44 +0530

    changed loading state

--- src/components/UI/PeopleItemsLoading.tsx
+++ src/components/UI/PeopleItemsLoading.tsx
@@ -1,53 +1,55 @@
 const PeopleItemsLoading = () => {
     return (
         <div>
-            <div className="flex flex-row animate-pulse  w-full px-3 lg:px-2 py-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
-                <div className="flex h-12 w-12 bg-slate-500 rounded-full"></div>
 
-                <div className="flex flex-col w-5/6 h-16 border-b-2 border-gray-200">
-                    <div className="flex flex-row w-full justify-between h-2 rounded-xl bg-slate-500"> </div>
+            <div className="flex flex-row animate-pulse items-center w-full px-3 lg:px-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
+                <div className="flex items-center w-12 h-12 bg-slate-500 rounded-full"></div>
+
+                <div className="flex flex-col justify-center w-5/6 h-16 border-b-2 border-gray-200">
+                    <p className="lg:text-lg h-2.5 rounded-xl bg-slate-500 font-semibold lg:font-bold w-full max-w-[80%] md:max-w-[70%] whitespace-nowrap text-ellipsis overflow-hidden"></p>
                 </div>
             </div>
 
-            <div className="flex flex-row animate-pulse  w-full px-3 lg:px-2 py-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
-                <div className="flex h-12 w-12 bg-slate-500 rounded-full"></div>
+            <div className="flex flex-row animate-pulse items-center w-full px-3 lg:px-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
+                <div className="flex items-center w-12 h-12 bg-slate-500 rounded-full"></div>
 
-                <div className="flex flex-col w-5/6 h-16 border-b-2 border-gray-200">
-                    <div className="flex flex-row w-full justify-between h-2 rounded-xl bg-slate-500"> </div>
+                <div className="flex flex-col justify-center w-5/6 h-16 border-b-2 border-gray-200">
+                    <p className="lg:text-lg h-2.5 rounded-xl bg-slate-500 font-semibold lg:font-bold w-full max-w-[80%] md:max-w-[70%] whitespace-nowrap text-ellipsis overflow-hidden"></p>
                 </div>
             </div>
 
-            <div className="flex flex-row animate-pulse  w-full px-3 lg:px-2 py-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
-                <div className="flex h-12 w-12 bg-slate-500 rounded-full"></div>
+            <div className="flex flex-row animate-pulse items-center w-full px-3 lg:px-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
+                <div className="flex items-center w-12 h-12 bg-slate-500 rounded-full"></div>
 
-                <div className="flex flex-col w-5/6 h-16 border-b-2 border-gray-200">
-                    <div className="flex flex-row w-full justify-between h-2 rounded-xl bg-slate-500"> </div>
+                <div className="flex flex-col justify-center w-5/6 h-16 border-b-2 border-gray-200">
+                    <p className="lg:text-lg h-2.5 rounded-xl bg-slate-500 font-semibold lg:font-bold w-full max-w-[80%] md:max-w-[70%] whitespace-nowrap text-ellipsis overflow-hidden"></p>
                 </div>
             </div>
 
-            <div className="flex flex-row animate-pulse  w-full px-3 lg:px-2 py-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
-                <div className="flex h-12 w-12 bg-slate-500 rounded-full"></div>
+            <div className="flex flex-row animate-pulse items-center w-full px-3 lg:px-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
+                <div className="flex items-center w-12 h-12 bg-slate-500 rounded-full"></div>
 
-                <div className="flex flex-col w-5/6 h-16 border-b-2 border-gray-200">
-                    <div className="flex flex-row w-full justify-between h-2 rounded-xl bg-slate-500"> </div>
+                <div className="flex flex-col justify-center w-5/6 h-16 border-b-2 border-gray-200">
+                    <p className="lg:text-lg h-2.5 rounded-xl bg-slate-500 font-semibold lg:font-bold w-full max-w-[80%] md:max-w-[70%] whitespace-nowrap text-ellipsis overflow-hidden"></p>
                 </div>
             </div>
 
-            <div className="flex flex-row animate-pulse  w-full px-3 lg:px-2 py-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
-                <div className="flex h-12 w-12 bg-slate-500 rounded-full"></div>
+            <div className="flex flex-row animate-pulse items-center w-full px-3 lg:px-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
+                <div className="flex items-center w-12 h-12 bg-slate-500 rounded-full"></div>
 
-                <div className="flex flex-col w-5/6 h-16 border-b-2 border-gray-200">
-                    <div className="flex flex-row w-full justify-between h-2 rounded-xl bg-slate-500"> </div>
+                <div className="flex flex-col justify-center w-5/6 h-16 border-b-2 border-gray-200">
+                    <p className="lg:text-lg h-2.5 rounded-xl bg-slate-500 font-semibold lg:font-bold w-full max-w-[80%] md:max-w-[70%] whitespace-nowrap text-ellipsis overflow-hidden"></p>
                 </div>
             </div>
 
-            <div className="flex flex-row animate-pulse  w-full px-3 lg:px-2 py-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
-                <div className="flex h-12 w-12 bg-slate-500 rounded-full"></div>
+            <div className="flex flex-row animate-pulse items-center w-full px-3 lg:px-2 rounded-xl space-x-2 hover:bg-gray-100 cursor-pointer">
+                <div className="flex items-center w-12 h-12 bg-slate-500 rounded-full"></div>
 
-                <div className="flex flex-col w-5/6 h-16 border-b-2 border-gray-200">
-                    <div className="flex flex-row w-full justify-between h-2 rounded-xl bg-slate-500"> </div>
+                <div className="flex flex-col justify-center w-5/6 h-16 border-b-2 border-gray-200">
+                    <p className="lg:text-lg h-2.5 rounded-xl bg-slate-500 font-semibold lg:font-bold w-full max-w-[80%] md:max-w-[70%] whitespace-nowrap text-ellipsis overflow-hidden"></p>
                 </div>
             </div>
+
         </div>
     )
 }
