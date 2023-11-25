2cb3db727e3a3475ef16fb9932a89a741395f059
unknown <aryan23dahiya@gmail.com>
2023-11-25 23:22:57 +0530

    login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -26,9 +26,9 @@ const Login = () => {
 
         <div className="flex justify-center items-center h-[100dvh] w-full bg-gray-300">
 
-            <div className="flex flex-col space-y-3 items-center h-full w-full pt-[40%] md:pt-[10%] lg:pt-5 rounded-xl md:h-3/5 md:w-3/5 lg:w-1/4 bg-white">
+            <div className="flex flex-col space-y-3 items-center h-full w-full pt-[30%] md:pt-[10%] lg:pt-5 rounded-xl md:h-3/5 md:w-3/5 lg:w-1/4 bg-white">
 
-                <div className="h-40 w-40 lg:h-1/5 lg:w-1/5">
+                <div className="h-32 w-32 lg:h-1/5 lg:w-1/5">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv44m_vIAb_65RiKNjaJwMfgaM-xi9DGBjxu2nimh5jYGqgdrN-NceWFL7eA4CKJ9emUA&usqp=CAU" 
                     alt="logo"
                     className="h-full w-full" />
