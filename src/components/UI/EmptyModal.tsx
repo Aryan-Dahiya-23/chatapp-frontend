bbe8bf68d1edd2d3e5d2757bffe921bf893f8778
Aryan Dahiya <aryan23dahiya@gmail.com>
2024-01-10 19:52:31 +0530

    added hello gif

--- src/components/UI/EmptyModal.tsx
+++ src/components/UI/EmptyModal.tsx
@@ -1,7 +1,10 @@
+import Hello from "../../assets/Hello.gif";
+
 const EmptyModal = () => {
     return (
         <div className="hidden justify-center items-center text-xl lg:text-3xl font-bold md:flex md:w-[52%] lg:w-[70%] border-l-2 border-gray-200">
-            Select a chat or start a new conversation
+            {/* Select a chat or start a new conversation */}
+            <img src={Hello} className="max-h-[95vh]" alt="Hello" />
         </div>
     )
 }
